5947f90e6d3da97d9f123c17f19d1db8e3618299
Igor Alves <bonfarj@gmail.com>
2018-09-16 23:09:34 +0200

    Refactoring the MacBookProPriceController for reusing the code.

--- src/main/java/com/igor/service/mac/macbookpro/MacBookProPriceController.java
+++ src/main/java/com/igor/service/mac/macbookpro/MacBookProPriceController.java
@@ -5,32 +5,49 @@ import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlHeading2;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.igor.model.Product;
-import com.igor.service.mac.MacPriceController;
+import com.igor.service.CountryRepository;
+import com.igor.service.mac.NewMacPriceController;
+import com.igor.xml.Country;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public abstract class MacBookProPriceController extends MacPriceController {
+@RestController
+public class MacBookProPriceController extends NewMacPriceController {
+
+    private static final String URL = "https://www.apple.com/%s/shop/buy-mac/macbook-pro";
 
     private static final String DATA_GROUP_13 = "13inch";
     private static final String DATA_GROUP_15 = "15inch";
 
-    protected List<Product> loadMacBooksPro() throws IOException {
+    private static final String TYPE_13_INCH = "13-inch";
+    private static final String TYPE_15_INCH = "15-inch";
 
-        final List<Product> products = getProducts(getType13Inch());
+    @RequestMapping("/{countryCode}/macbookpro")
+    @Cacheable("macbookpro")
+    public List<Product> getProducts(@PathVariable String countryCode) throws IOException {
 
-        products.addAll(getProducts(getType15Inch()));
+        final List<Product> products = getProducts(getType13Inch(), countryCode);
+
+        products.addAll(getProducts(getType15Inch(), countryCode));
 
         return products;
     }
 
-    private List<Product> getProducts(String type) throws IOException {
+    private List<Product> getProducts(String type, String countryCode) throws IOException {
+
+        final Country country = new CountryRepository().getCountry(countryCode);
+        final String url = getURL(countryCode);
 
         final WebClient client = getClient();
 
-        final HtmlPage page = client.getPage(getURL());
+        final HtmlPage page = client.getPage(url);
         final String dataGroup;
 
         if (type.equals(getType13Inch())) {
@@ -57,12 +74,20 @@ public abstract class MacBookProPriceController extends MacPriceController {
                 .map(HtmlHeading2.class::cast)
                 .findFirst().get().getTextContent().trim();
 
-        return createProducts(tags, modelPrefix);
+        return createProducts(tags, modelPrefix, country);
     }
 
-    protected abstract String getType13Inch();
+    private String getURL(String countryCode) {
+        return String.format(URL, countryCode);
+    }
 
-    protected abstract String getType15Inch();
+    private String getType13Inch() {
+        return TYPE_13_INCH;
+    }
+
+    private String getType15Inch() {
+        return TYPE_15_INCH;
+    }
 
 
 }
